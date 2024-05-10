471b97d417f54752fb7516391d6b3b345d0d9ce2
Vitor Ramires <vmr.rs@hotmail.com>
2024-05-10 13:18:36 -0300

    feat: animations and responsive window of projects page

--- js/script.js
+++ js/script.js
@@ -3,16 +3,37 @@ const mobileMenu = document.querySelector('.menu-mobile .links')
 const closeMenu = document.querySelector('.close')
 const openMenu = document.querySelector('.open')
 const linksMenuMobile = document.querySelectorAll('.links ul li')
+const animationTargets = document.querySelectorAll('.animation-target')
 
 hamburguerIcon.addEventListener('click', ()=>{
-    mobileMenu.classList.toggle('menu-opened')
-    closeMenu.classList.toggle('show-close-btn')
-    openMenu.classList.toggle('show-open-btn')
-    linksMenuMobile.forEach(link => {
-        link.addEventListener('click', ()=>{
-            mobileMenu.classList.remove('menu-opened')
-        })
-    });
+  mobileMenu.classList.toggle('menu-opened')
+  closeMenu.classList.toggle('show-close-btn')
+  openMenu.classList.toggle('show-open-btn')
+  linksMenuMobile.forEach(link => {
+    link.addEventListener('click', ()=>{
+      mobileMenu.classList.remove('menu-opened')
+      closeMenu.classList.add('show-close-btn')
+      openMenu.classList.add('show-open-btn')
+    })
+  });
 })
 
-console.log('teste')
+
+function handlerScroll(){
+  animationTargets.forEach(target=>{
+    let elementFromTop = target.getBoundingClientRect().top
+    let windowHeight = window.innerHeight * 0.7
+    if(elementFromTop < windowHeight){
+      target.classList.add('active-animation')
+    } else if (elementFromTop > windowHeight){
+      target.classList.remove('active-animation')
+    }
+  })
+}
+
+setTimeout(()=>{
+  handlerScroll()
+}, 250)
+
+window.addEventListener('scroll', handlerScroll)
+
