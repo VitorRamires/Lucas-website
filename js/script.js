5f2884eac84028646e9fefa7e79c7df09c6483c0
Vitor Ramires <vmr.rs@hotmail.com>
2024-05-10 09:24:51 -0300

    feat: close menu when click on link

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@ const hamburguerIcon = document.querySelector('.hamburguer')
 const mobileMenu = document.querySelector('.menu-mobile .links')
 const closeMenu = document.querySelector('.close')
 const openMenu = document.querySelector('.open')
+const linksMenuMobile = document.querySelectorAll('.links ul li')
 
 hamburguerIcon.addEventListener('click', ()=>{
     mobileMenu.classList.toggle('menu-opened')
     closeMenu.classList.toggle('show-close-btn')
     openMenu.classList.toggle('show-open-btn')
+    linksMenuMobile.forEach(link => {
+        link.addEventListener('click', ()=>{
+            mobileMenu.classList.remove('menu-opened')
+        })
+    });
 })
 
 console.log('teste')
