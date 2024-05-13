73c27d28301e30063afd5055a9ebf662154f5f4a
Vitor Ramires <vmr.rs@hotmail.com>
2024-05-13 11:51:37 -0300

    feat: tecnologys and animations on project pages

--- js/script.js
+++ js/script.js
@@ -5,24 +5,27 @@ const openMenu = document.querySelector('.open')
 const linksMenuMobile = document.querySelectorAll('.links ul li')
 const animationTargets = document.querySelectorAll('.animation-target')
 
-hamburguerIcon.addEventListener('click', ()=>{
-  mobileMenu.classList.toggle('menu-opened')
-  closeMenu.classList.toggle('show-close-btn')
-  openMenu.classList.toggle('show-open-btn')
-  linksMenuMobile.forEach(link => {
-    link.addEventListener('click', ()=>{
-      mobileMenu.classList.remove('menu-opened')
-      closeMenu.classList.add('show-close-btn')
-      openMenu.classList.add('show-open-btn')
-    })
-  });
-})
+
+if(hamburguerIcon){
+  hamburguerIcon.addEventListener('click', ()=>{
+    mobileMenu.classList.toggle('menu-opened')
+    closeMenu.classList.toggle('show-close-btn')
+    openMenu.classList.toggle('show-open-btn')
+    linksMenuMobile.forEach(link => {
+      link.addEventListener('click', ()=>{
+        mobileMenu.classList.remove('menu-opened')
+        closeMenu.classList.add('show-close-btn')
+        openMenu.classList.add('show-open-btn')
+      })
+    });
+  })
+}
 
 
 function handlerScroll(){
   animationTargets.forEach(target=>{
     let elementFromTop = target.getBoundingClientRect().top
-    let windowHeight = window.innerHeight * 0.85
+    let windowHeight = window.innerHeight * 0.9
     if(elementFromTop < windowHeight){
       target.classList.add('active-animation')
     } else if (elementFromTop > windowHeight){
